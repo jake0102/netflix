a7e9d8949c9ec1e7a4f3a88bf6f5a9e481347cd2
jake0102 <jakejapan0102@gmail.com>
2022-03-14 13:28:35 +0900

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.scss'
 import HomePage from './Pages/Home'
 import AboutPage from './Pages/About'
+import ProductPage from './Pages/Product'
+import ContactPage from './Pages/Contact'
 
 function App() {
   return (
     <div className="App">
       <HomePage />
       <AboutPage />
+      <ProductPage />
+      <ContactPage />
     </div>
-  );
+  )
 }
 
 export default App;
