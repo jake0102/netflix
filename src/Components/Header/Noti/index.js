30ab40776c2fb2797ad00c5f3fbd69ac608c6ce8
jake0102 <jakejapan0102@gmail.com>
2022-03-14 20:38:38 +0900

    new

--- src/Components/Header/Noti/index.js
+++ src/Components/Header/Noti/index.js
@@ -1,10 +1,30 @@
+import { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function Noti(){
+
+    const [show, setShow] = useState(false)
+
+    const searchRef = useRef()
+
+    useEffect(()=>{
+
+
+        // Cleanup function
+        return () => {
+
+        }
+    })
+
     return(
-        <Link className="header__noti" to="/Register" ><FontAwesomeIcon icon={faBell} /></Link>
+        <div className="header__other">
+            <div className="header__other__search" >
+                <FontAwesomeIcon icon={faSearch} />
+            </div>
+            <Link className="header__other__noti" to="/Register" ><FontAwesomeIcon icon={faBell} /></Link>
+        </div>
     )
 }
 
