eaa9a92538d27acc5eb1231287f2d8e73e482e89
jake0102 <jakejapan0102@gmail.com>
2022-03-14 15:01:09 +0900

    new

--- src/Components/Header/Nav/index.js
+++ src/Components/Header/Nav/index.js
@@ -1,19 +1,33 @@
 function Nav(){
+
+    const handleActive = (e) => {
+
+        const activedItem = document.querySelector('.header__nav__item.active')
+
+        activedItem.classList.remove('active')
+
+        e.target.classList.add('active')
+    }
+
     return(
         <div className="header__nav">
-            <div className="header__nav__item active">
+            <div className="header__nav__item active" onClick={(e) => handleActive(e)}>
                 Home
             </div>
 
-            <div className="header__nav__item">
+            <div className="header__nav__item" onClick={(e) => handleActive(e)}>
                 Product
             </div>
 
-            <div className="header__nav__item">
+            <div className="header__nav__item" onClick={(e) => handleActive(e)}>
+                Blog
+            </div>
+
+            <div className="header__nav__item" onClick={(e) => handleActive(e)}>
                 Contact
             </div>
 
-            <div className="header__nav__item">
+            <div className="header__nav__item" onClick={(e) => handleActive(e)}>
                 About
             </div>
         </div>
