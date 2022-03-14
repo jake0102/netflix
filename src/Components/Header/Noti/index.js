6d81749348ed14edb0fb9e5cbebbf33486bec25c
jake0102 <jakejapan0102@gmail.com>
2022-03-14 21:46:25 +0900

    new

--- src/Components/Header/Noti/index.js
+++ src/Components/Header/Noti/index.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell, faSearch } from '@fortawesome/free-solid-svg-icons'
@@ -7,23 +7,37 @@ function Noti(){
 
     const [show, setShow] = useState(false)
 
-    const searchRef = useRef()
-
     useEffect(()=>{
+        
+        const search = document.querySelector('.header__other__search')
 
-
-        // Cleanup function
-        return () => {
-
+        if(show){
+            if(search.classList[1] == 'search__Hide'){
+                search.classList.remove('search__Hide')
+            }
+            search.classList.add('search__Show')
+        }else{
+            if(search.classList[1] == 'search__Show'){
+                search.classList.remove('search__Show')
+            }
+            search.classList.add('search__Hide')
         }
-    })
+    }, [show])
 
     return(
         <div className="header__other">
             <div className="header__other__search" >
-                <FontAwesomeIcon icon={faSearch} />
+                <FontAwesomeIcon 
+                    icon={faSearch} 
+                    style={{margin: '0 10px 0 10px'}} 
+                    onClick={() => setShow(!show)}
+                />
+                <input type="text" placeholder="Search..."/>
             </div>
-            <Link className="header__other__noti" to="/Register" ><FontAwesomeIcon icon={faBell} /></Link>
+
+            <Link className="header__other__noti" to="/Register" >
+                <FontAwesomeIcon icon={faBell} />
+            </Link>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import { ThemeProvider } from './ThemeContext'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
