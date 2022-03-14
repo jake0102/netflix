b54474a7b29c5515a3f44510e75a3fb4c02521e6
jake0102 <jakejapan0102@gmail.com>
2022-03-14 15:33:04 +0900

    new

--- src/Components/Header/Noti/index.js
+++ src/Components/Header/Noti/index.js
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
 
 function Noti(){
     return(
-        <Link className="header__noti" to="/Register"><FontAwesomeIcon icon={faCoffee} /></Link>
+        <Link className="header__noti" to="/Register"><FontAwesomeIcon icon={faBell} /></Link>
     )
 }
 
