abbb70c965c680b4d6f7ac62ccd652ac9054f477
jake0102 <jakejapan0102@gmail.com>
2022-03-18 16:03:23 +0900

    new

---
+++ src/Components/FirstLook/Right/Contents/index.js
@@ -0,0 +1,18 @@
+function Contents(){
+    return (
+        <div className="home-firstlook-right-description">
+            <div>
+                <div className="home-firstlook-right-description-container-title">
+                    <span>I'M STEVE MILNER. </span><br /> 
+                    <span>WEB DESIGNER</span>
+                </div>
+
+                <div className="home-firstlook-right-description-contents">
+                    <p>I'm a American based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contents
--- src/Components/FirstLook/Right/index.js
+++ src/Components/FirstLook/Right/index.js
@@ -1,22 +1,11 @@
+import Contents from './Contents'
+import Nav from './Nav'
+
 function Right(){
     return(
         <div className="home-firstlook-right">
-            <div className="home-firstlook-right-description">
-                <div>
-                    <div className="home-firstlook-right-description-container-title">
-                        <span>I'M STEVE MILNER. </span><br /> 
-                        <span>WEB DESIGNER</span>
-                    </div>
-
-                    <div className="home-firstlook-right-description-contents">
-                        <p>I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="home-firstlook-right-nav">
-
-            </div>
+            <Contents />
+            <Nav />
         </div>
     )
 }
