a257c34f4f025fd4aeeea37b6fb8ba717b9b60ca
jake0102 <jakejapan0102@gmail.com>
2022-03-20 21:22:05 +0900

    new

--- src/Components/MoreAboutMe/index.js
+++ src/Components/MoreAboutMe/index.js
@@ -1,7 +1,34 @@
+import { useContext } from 'react'
+
+import { ThemeContext } from '../../ThemeContext'
+
 function MoreAboutMe(){
+
+    const value = useContext(ThemeContext)
+
     return (
-        <div className="moreAboutMe">
-            <h1>MoreAboutMe</h1>
+        <div className={`moreAboutMe-container ${value.moreAboutMeTheme}`}>
+            <div className={`moreAboutMe-container-1 ${value.moreAboutMeTheme1}`} >
+                <div className={`moreAboutMe-container-2 ${value.moreAboutMeTheme2}`}>
+                    <div className='moreAboutMe-container-3'>
+                        <div className='moreAboutMe-left'>
+                            <div className='moreAboutMe-left-container'>
+                                left
+                            </div>
+                        </div>
+                        <div className='moreAboutMe-right'>
+                            <div className='moreAboutMe-right-container'>
+                                <div className='moreAboutMe-right-item'>1</div>
+                                <div className='moreAboutMe-right-item'>2</div>
+                            </div>
+                            <div className='moreAboutMe-right-container'>
+                                <div className='moreAboutMe-right-item'>3</div>
+                                <div className='moreAboutMe-right-item'>4</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -11,11 +11,20 @@ function ThemeProvider( {children} ){
 
     const [rightNavTheme, setRightNavTheme] = useState('rightNav-light')
 
+    const [moreAboutMeTheme, setMoreAboutMeTheme] = useState('moreAboutMe-light')
+
+    const [moreAboutMeTheme1, setMoreAboutMeTheme1] = useState('moreAboutMe1-light')
+
+    const [moreAboutMeTheme2, setMoreAboutMeTheme2] = useState('moreAboutMe2-light')
+
     const toggleHeaderTheme = () => {
         setHeaderTheme(headerTheme === 'header-light' ? 'header-dark' : 'header-light')
         setNavTheme(navTheme === 'nav-light' ? 'nav-dark' : 'nav-light')
         setWrapperTheme(wrapperTheme === 'wrapper-light' ? 'wrapper-dark' : 'wrapper-light')
         setRightNavTheme(rightNavTheme === 'rightNav-light' ? 'rightNav-dark' : 'rightNav-light')
+        setMoreAboutMeTheme(moreAboutMeTheme === 'moreAboutMe-light' ? 'moreAboutMe-dark' : 'moreAboutMe-light')
+        setMoreAboutMeTheme1(moreAboutMeTheme1 === 'moreAboutMe1-light' ? 'moreAboutMe1-dark' : 'moreAboutMe1-light')
+        setMoreAboutMeTheme2(moreAboutMeTheme2 === 'moreAboutMe2-light' ? 'moreAboutMe2-dark' : 'moreAboutMe2-light')
     }
 
     const value = {
@@ -23,6 +32,9 @@ function ThemeProvider( {children} ){
         navTheme,
         wrapperTheme,
         rightNavTheme,
+        moreAboutMeTheme,
+        moreAboutMeTheme1,
+        moreAboutMeTheme2,
         toggleHeaderTheme
     }
 
