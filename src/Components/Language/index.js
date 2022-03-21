57bf3a15413a36a4031b4907d985fed8261f4bfc
jake0102 <jakejapan0102@gmail.com>
2022-03-21 15:19:05 +0900

    new

--- src/Components/Language/index.js
+++ src/Components/Language/index.js
@@ -1,44 +1,18 @@
 function Language(){
     return (
         <div className="language">
-            <div class="radialProgressBar progress-10">
-                <div class="overlay">10</div>
+            {/* <div class="c100 p12">
+                <span>12%</span>
+                <div class="slice">
+                    <div class="bar"></div>
+                    <div class="fill"></div>
+                </div>
+            </div> */}
+            <div className="language-top">
+                MY SKILLS
             </div>
+            <div className="language-bot">
 
-            <div class="radialProgressBar progress-20">
-                <div class="overlay">20</div>
-            </div>
-
-            <div class="radialProgressBar progress-30">
-                <div class="overlay">30</div>
-            </div>
-
-            <div class="radialProgressBar progress-40">
-                <div class="overlay">40</div>
-            </div>
-
-            <div class="radialProgressBar progress-50">
-                <div class="overlay">50</div>
-            </div>
-
-            <div class="radialProgressBar progress-60">
-                <div class="overlay">60</div>
-            </div>
-
-            <div class="radialProgressBar progress-70">
-                <div class="overlay">70</div>
-            </div>
-
-            <div class="radialProgressBar progress-80">
-                <div class="overlay">80</div>
-            </div>
-
-            <div class="radialProgressBar progress-90">
-                <div class="overlay">90</div>
-            </div>
-
-            <div class="radialProgressBar progress-100">
-                <div class="overlay">100</div>
             </div>
         </div>
     )
