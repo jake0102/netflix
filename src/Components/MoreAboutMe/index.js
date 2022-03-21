5902e536e45bf6f5e5d2062f90375ab684b06f38
jake0102 <jakejapan0102@gmail.com>
2022-03-21 11:23:18 +0900

    new

--- src/Components/MoreAboutMe/index.js
+++ src/Components/MoreAboutMe/index.js
@@ -13,17 +13,56 @@ function MoreAboutMe(){
                     <div className='moreAboutMe-container-3'>
                         <div className='moreAboutMe-left'>
                             <div className='moreAboutMe-left-container'>
-                                left
+                                <div className="moreAboutMe-left-container-title">
+                                    PERSONAL INFOS
+                                </div>
+
+                                <div className="moreAboutMe-left-container-contents">
+                                    <div className="moreAboutMe-left-container-contents-left">
+                                        <ul>
+                                            <li>First Name: <span style={{fontWeight: "bold"}}>Steve</span></li>
+                                            <li>Age: <span style={{fontWeight: "bold"}}>27 Years</span></li>
+                                            <li>Freelance: <span style={{fontWeight: "bold"}}>Available</span></li>
+                                            <li>Phone: <span style={{fontWeight: "bold"}}>+21621184010</span></li>
+                                            <li>Skype: <span style={{fontWeight: "bold"}}>steve.milner</span></li>
+                                        </ul>
+                                    </div>
+                                    <div className="moreAboutMe-left-container-contents-right">
+                                        <ul>
+                                            <li>Last Name: <span style={{fontWeight: "bold"}}>Milner</span></li>
+                                            <li>Nationality: <span style={{fontWeight: "bold"}}>Tunisian</span></li>
+                                            <li>Addness: <span style={{fontWeight: "bold"}}>Tunis</span></li>
+                                            <li>Email: <span style={{fontWeight: "bold"}}>you@mail.com</span></li>
+                                            <li>Languages: <span style={{fontWeight: "bold"}}>French, English</span></li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className='moreAboutMe-right'>
                             <div className='moreAboutMe-right-container'>
-                                <div className='moreAboutMe-right-item'>1</div>
-                                <div className='moreAboutMe-right-item'>2</div>
+                                <div className='moreAboutMe-right-item'>
+                                    <div className="x">
+
+                                    </div>
+                                </div>
+                                <div className='moreAboutMe-right-item'>
+                                    <div className="x">
+
+                                    </div>
+                                </div>
                             </div>
                             <div className='moreAboutMe-right-container'>
-                                <div className='moreAboutMe-right-item'>3</div>
-                                <div className='moreAboutMe-right-item'>4</div>
+                                <div className='moreAboutMe-right-item'>
+                                    <div className="x">
+
+                                    </div>
+                                </div>
+                                <div className='moreAboutMe-right-item'>
+                                    <div className="x">
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
