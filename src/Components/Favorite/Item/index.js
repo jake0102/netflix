f25eac2f32df3853d7be3076a34dd70ce8e0334d
jake0102 <jakejapan0102@gmail.com>
2022-03-22 08:50:32 +0900

    new

---
+++ src/Components/Favorite/Item/index.js
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import img1 from '../list.png'
+
+function Item(props){
+
+
+
+    return (
+        <div className="favorite-container mb-5">
+            <div className="favorite-container-tilte">
+                <span style={{color: props.data.color}} >{props.data.title}</span>
+                <FontAwesomeIcon icon={props.data.icon} style={{marginLeft: '1rem', color: props.data.color}}/>
+            </div>
+                
+            <div className="favorite-container-content mt-3">
+                <ul>
+                    {props.data.content.map((item, index) => (
+                        <li><img src={img1} className="me-3" key={index}/>{item}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Item
--- src/Components/Favorite/index.js
+++ src/Components/Favorite/index.js
@@ -3,11 +3,39 @@ import { faBasketball, faMusic, faGamepad, faFlag } from '@fortawesome/free-soli
 import { useContext } from 'react'
 
 import { ThemeContext } from '../../ThemeContext'
-import img1 from './football.png'
+import Item from './Item'
 
 function Favorite(){
 
     const value = useContext(ThemeContext)
+
+    const sport = {
+        color: 'rgb(218, 105, 105)',
+        title: 'Sport',
+        icon: faBasketball,
+        content: ['Succer', 'Swim', 'Marathon']
+    }
+
+    const music = {
+        color: '#f5c868',
+        title: 'Music',
+        icon: faMusic,
+        content: ['Intention - Justin Bieber', '7 rings - Ariana Grande', 'Perfect - Ed Sheeran']
+    }
+
+    const game = {
+        color: '#8abc43',
+        title: 'Game',
+        icon: faGamepad,
+        content: ['Valorant', 'League of legends', 'Naraka bladepoint']
+    }
+
+    const nation = {
+        color: '#339dcc',
+        title: 'Nation',
+        icon: faFlag,
+        content: ['Japan', 'United of states', 'Korea']
+    }
 
     return (
         <div className="favorite">
@@ -15,67 +43,15 @@ function Favorite(){
                 <span>MY FAVORITE</span>
             </div>
 
-            <div className="favorite-container mb-5">
-                <div className="favorite-container-tilte">
-                    <span style={{color: 'rgb(218, 105, 105)'}} >Sport</span>
-                    <FontAwesomeIcon icon={faBasketball} style={{marginLeft: '1rem', color: 'rgb(218, 105, 105)'}}/>
-                </div>
-                
-                <div className="favorite-container-content mt-3">
-                    <ul>
-                        <li><img src={img1} className="me-3"/>Succer</li>
-                        <li><img src={img1} className="me-3"/>Swim</li>
-                        <li><img src={img1} className="me-3"/>Marathon</li>
-                    </ul>
-                </div>
-            </div>
+            <Item data={sport}/>
 
-            <div className="favorite-container mb-5">
-                <div className="favorite-container-tilte">
-                    <span style={{color: '#f5c868'}} >Music</span>
-                    <FontAwesomeIcon icon={faMusic} style={{marginLeft: '1rem', color: '#f5c868'}}/>
-                </div>
-                
-                <div className="favorite-container-content mt-3">
-                    <ul>
-                        <li><img src={img1} className="me-3"/>Intention - Justin Bieber</li>
-                        <li><img src={img1} className="me-3"/>7 rings - Ariana Grande</li>
-                        <li><img src={img1} className="me-3"/>Perfect - Ed Sheeran</li>
-                    </ul>
-                </div>
-            </div>
-            
-            <div className="favorite-container mb-5">
-                <div className="favorite-container-tilte">
-                    <span style={{color: '#8abc43'}} >Game</span>
-                    <FontAwesomeIcon icon={faGamepad} style={{marginLeft: '1rem', color: '#8abc43'}}/>
-                </div>
-                
-                <div className="favorite-container-content mt-3">
-                    <ul>
-                        <li><img src={img1} className="me-3"/>Valorant</li>
-                        <li><img src={img1} className="me-3"/>League of legends</li>
-                        <li><img src={img1} className="me-3"/>Naraka bladepoint</li>
-                    </ul>
-                </div>
-            </div>
+            <Item data={music} />
 
-            <div className="favorite-container">
-                <div className="favorite-container-tilte">
-                    <span style={{color: '#339dcc'}} >Nation</span>
-                    <FontAwesomeIcon icon={faFlag} style={{marginLeft: '1rem', color: '#339dcc'}}/>
-                </div>
-                
-                <div className="favorite-container-content mt-3">
-                    <ul>
-                        <li><img src={img1} className="me-3"/>Japan</li>
-                        <li><img src={img1} className="me-3"/>United States</li>
-                        <li><img src={img1} className="me-3"/>Korea</li>
-                    </ul>
-                </div>
-            </div>
+            <Item data={game} />
 
-            <div className={value.headerTheme} style={{height: '50px'}}></div>
+            <Item data={nation} />
+
+            <div className={value.headerTheme} style={{height: '10px'}}></div>
         </div>
     )
 }
